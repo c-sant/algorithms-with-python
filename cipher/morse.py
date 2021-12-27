78f835d6d67341bed66640d0f435ebb305d3ea3f
Carlos <cadu.vs@hotmail.com>
2021-12-27 15:52:33 -0300

    added morse module to cipher package

---
+++ cipher/morse.py
@@ -0,0 +1,68 @@
+MORSE_ALPHABET = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-...',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '.': '.-.-.-', ',': '--..--', '?': '..--..',
+    '/': '--..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...',
+    ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-',
+    '"': '.-..-.', '$': '...-..-', '@': '.--.-.', '0': '-----', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.', ' ': ''
+}
+
+def to_morse(char: str, alphabet: dict = MORSE_ALPHABET):
+    """
+    Translates an individual character into it's morse code equivalent.
+    """
+    char = str(char)
+
+    return alphabet[char]
+
+def from_morse(char: str, alphabet: dict = None):
+    """
+    Translates an individual morse code character into it's Latin alphabet
+    equivalent.
+    """
+    if alphabet == None:
+        alphabet = dict(zip(MORSE_ALPHABET.values(), MORSE_ALPHABET.keys()))
+    
+    char = str(char)
+
+    return alphabet[char]
+
+def text_to_morse(text: str):
+    """
+    Converts text into morse code.
+    """
+    text = text.upper()
+    text = list(text)
+
+    for i in range(len(text)):
+        text[i] = to_morse(text[i])
+
+    return ' '.join(text)
+
+def text_from_morse(text: str):
+    """
+    Translates text from morse code.
+    """
+    alphabet = dict(zip(MORSE_ALPHABET.values(), MORSE_ALPHABET.keys()))
+
+    text = text.split(' ')
+    
+    for i in range(len(text)):
+        text[i] = from_morse(text[i], alphabet)
+
+    return ''.join(text)
+
+if __name__ == '__main__':
+    text = 'Dummy text'
+    
+    text = text_to_morse(text)
+
+    print(text)
+
+    text = text_from_morse(text)
+
+    print(text) # DUMMY TEXT (original text in uppercase)
